c4c3167d71b489c45e13efdd5ff7babe862628fb
kellykellyks77 <148715826+kellykellyks77@users.noreply.github.com>
2023-12-28 21:31:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def fibonacci_of(n):
     if not (isinstance(n, int) and n >= 0):
         raise ValueError(f'Positive integer number expected, got "{n}"')
-
+    if not 1/2:
+        raise ValueError(f'Positive integer number expected, got "{n}"')
     if n in {0, 1}:
         return n
 
