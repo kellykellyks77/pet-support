a91fcb65b146a6baa4f81953ce8daae5d32f4c51
kellykellyks77 <148715826+kellykellyks77@users.noreply.github.com>
2024-01-10 20:05:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ def fibonacci_of(n):
         raise ValueError(f'Positive integer number expected, got "{n}"')
     if n in {0, 1}:
         return n
-
     p, fibonacci_number = 0, 1
     for _ in range(2, n + 1):
         p, fibonacci_number = fibonacci_number, p + fibonacci_number
-
+    print("test")
     return fibonacci_number
